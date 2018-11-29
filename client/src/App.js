994ba186bb909bc8990d7a4d463bf35f567237b2
VishalRajole <vishal.rajole@mobikontech.com>
2018-11-29 23:37:07 +0530

    initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    response: ''
+  };
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+  callApi = async () => {
+    const response = await fetch('/users');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
   render() {
     return (
       <div className="App">
@@ -19,6 +33,8 @@ class App extends Component {
           >
             Learn React
           </a>
+          <p>{this.state.response}</p>
+
         </header>
       </div>
     );
